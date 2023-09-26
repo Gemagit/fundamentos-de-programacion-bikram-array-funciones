53dd82d817ac2ba1602fb34102e7331694733b81
Gemagit <gemitaphotographer@gmail.com>
2023-09-26 23:04:17 +0200

    /addindex.js

---
+++ index.js
@@ -0,0 +1,99 @@
+const arrayVacio= [];
+
+const arrayNumeros= [0,1,2,3,4,5,6,7,8,9];
+
+const arrayNumerosPares= [0,2,4,6,8,];
+
+const arrayBidimensional= [[0,1,2],["a","b","c"]];
+
+function suma (a,b) {
+     return a + b;
+}
+
+function potenciacion (a,b){
+    for (let i = 0; i < b; i++) {
+        return a**2;
+      }
+}
+
+function separarPalabras () {
+    for (let i="a"; i=="a";i++ ){
+        return array("holaquetal?","TheBridgeforlife", "Avercomoseparasesto") ;
+    }
+}
+
+function repetirString ( "a",b);{
+    return repetirString ("a" + b++);
+}
+
+
+
+function esPrimo () {
+    for ( let i=2; i>2; i++) {
+        return true | false;
+    }
+}
+
+function ordenarArray () {
+    for (let i=0; i==0; i++){
+        return ordenarArray.sort ;
+    }
+    
+}
+
+function obtenerPares () {
+    for (let i=o;i>=0; i++){
+        return i >0;
+    }
+}
+
+function pintarArray () {
+    for (let i=0;i>0; i++){
+        return [0,1,2]+"[0,1,2]";
+    }
+}
+
+function arrayMapi(array) {
+    var arrayanswer = [];
+    for (let i = 0; i < array.length; i++) {
+        arrayanswer.push(sumaruse(array[i]));
+    }
+    return arrayanswer;
+}
+
+function eliminarDuplicados (array) {
+    array.push (eliminarDuplicados);
+}
+
+let arrayNumerosNeg = [0,-1,-2,-3,-4,-5,-6,-7,-8,-9];
+
+let "hola mundo"= ["hola", "Mundo"];
+
+let loGuardoTodo= ["hola","que",23,42.33 ,"tal"];
+
+let arrayDeArrays= [[756,"nombre"],[225,"apellido"],[298,"dirección"]];
+
+function multiplicacion (a,b) {
+    return a*b;
+}
+
+function division (a,b);{
+    return a/b;
+}
+
+function esPar (a) {
+    return true(a%2) || false(a%3);
+}
+
+ let arrayFunciones = [array ];
+ function operaciones (a+b (+,-,%)){
+    return resultado;
+ }
+    
+ function ordenarArray2 (a,b){
+    if (a>b) return 1;
+    if (a<b) return -1;
+ }
+
+
+
